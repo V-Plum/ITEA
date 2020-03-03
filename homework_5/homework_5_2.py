470524534f50807b830e6930048552737e8b6026
V-Plum <you@example.com>
2020-03-03 21:47:49 +0200

    Homework 5 task 2

--- homework_5/homework_5_2.py
+++ homework_5/homework_5_2.py
@@ -7,9 +7,9 @@ except ValueError:
 f = [0,1]
 
 # Put n Fibonacci numbers to list
-for i in range(1, quantity):
-    f_next = f[i] + f[i-1]
+for i in range(0, quantity):
+    f_next = f[i+1] + f[i]
     f.append(f_next)
 
 # Print result
-print(f[1:quantity])
+print(f[1:quantity+1])
