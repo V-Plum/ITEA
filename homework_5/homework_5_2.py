ac86b4c60cca87fe979660fc213d771b75df3c4c
V-Plum <you@example.com>
2020-03-03 21:39:08 +0200

    Homework 5 task 2

---
+++ homework_5/homework_5_2.py
@@ -0,0 +1,15 @@
+try:
+    quantity = int(input("How many Febonacci numbers do you want to see? "))
+except ValueError:
+    print("That's not a number")
+
+# Start
+f = [0,1]
+
+# Put n Fibonacci numbers to list
+for i in range(1, quantity):
+    f_next = f[i] + f[i-1]
+    f.insert(i+1, f_next)
+
+# Print result
+print(f[1:quantity])
