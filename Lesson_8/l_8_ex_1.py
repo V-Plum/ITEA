3f84907b4b4466c7a11824cea0a3b2d29ba3325f
V-Plum <v.v.plum@gmail.com>
2020-03-16 19:46:36 +0200

    Lesson 8 ex 1

---
+++ Lesson_8/l_8_ex_1.py
@@ -0,0 +1,21 @@
+"""
+У введеному реченні змінити регістр всіх букв. Наприклад:
+рядок “I`m eaTinG BaNaNa.” має бути конвертований у “i`M EAtINg bAnAnA.”
+
+"""
+
+def change_case(in_text):
+    out_text = ""
+    for i in range(len(in_text)-1):
+        current_letter = in_text[i]
+        if current_letter.isupper():
+            out_text += current_letter.lower()
+        elif current_letter.islower():
+            out_text += current_letter.upper()
+        else:
+            out_text += current_letter
+    return out_text
+
+in_text = "I`m eaTinG BaNaNa."
+out_text = change_case(in_text)
+print(out_text)
