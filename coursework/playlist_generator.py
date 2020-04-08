fc79193ad0add0b26dd610f423c5c3a4b028da29
V-Plum <you@example.com>
2020-04-08 14:38:46 +0300

    Code cleanup

--- coursework/playlist_generator.py
+++ coursework/playlist_generator.py
@@ -76,6 +76,18 @@ def main():
         [sg.Text(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.",
                  key="pld")]
     ]
+
+    # Switch to this layout to work wit PySimpleGUI instead of PySimpleGUIQt
+    # playlists_layout = [
+    #     [sg.Column(butt_lo1), sg.Listbox(pl1, key='pl1', size=(40, 10), select_mode="single")],
+    #     [sg.Column(butt_lo2), sg.Listbox(pl2, key='pl2', size=(40, 10), select_mode="multiple")],
+    #     [sg.Column(butt_lo3), sg.Listbox(pl3, key='pl3', size=(40, 10), select_mode="multiple")],
+    #     [sg.Column(butt_lo4), sg.Listbox(pl4, key='pl4', size=(40, 10), select_mode="multiple")],
+    #     [sg.Column(butt_lo5), sg.Listbox(pl5, key='pl5', size=(40, 10), select_mode="multiple")],
+    #     [sg.Text(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.",
+    #              key="pld")]
+    # ]
+
     layout = [
         [sg.Menu(menu_def, tearoff=False, pad=(200, 1))],
         [sg.Frame('All files', all_files_layout, font='Any 12', title_color='yellow', visible=True),
@@ -87,8 +99,11 @@ def main():
     window = sg.Window('Playlist creator v0.1', layout)
     while True:
         event, values = window.read()
-        if event is None:
-            break
+        if event is None or event == "Exit":
+            exit()
+
+        # Add files from a source folder
+
         elif event == "Add Folder":
             src = pg_ui.open_folder_dialog("Select source folder", path)
             if not src:
@@ -108,33 +123,60 @@ def main():
             window['td'].update(f"Total files duration: {src_dur // 60} min. {src_dur - (src_dur // 60) * 60} sec.")
             settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
             pg_actions.save_state(files_list, settings)
+
+        # Save all playlist sections to one playlist.m3u file
+
         elif event == "Generate Playlist":
             pg_actions.create_playlist(pl1+pl2+pl3+pl4+pl5, path)
             sg.popup(f"{len(pl1+pl2+pl3+pl4+pl5)} files added to playlist.m3u at {path}")
-        elif event == "Exit":
-            exit()
 
         # Remove items from playlist sections:
+        # It removes FIRST track with selected name from the section,
+        # because .GetIndexes() method temporary doesn't work with PySimpleGUIQt
 
         elif event == "rm1":
             track = values["pl1"][0]
             if track:
                 pl1.remove(track)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             pl_dur -= files_list[track][0]
             window['pl1'].update(pl1)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
+        elif event == "rm2":
+            track = values["pl2"][0]
+            if track:
+                pl2.remove(track)
+            pl_dur -= files_list[track][0]
+            window['pl2'].update(pl2)
+            window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
+        elif event == "rm3":
+            track = values["pl3"][0]
+            if track:
+                pl3.remove(track)
+            pl_dur -= files_list[track][0]
+            window['pl3'].update(pl3)
+            window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
+        elif event == "rm4":
+            track = values["pl4"][0]
+            if track:
+                pl4.remove(track)
+            pl_dur -= files_list[track][0]
+            window['pl4'].update(pl4)
+            window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
+        elif event == "rm5":
+            track = values["pl5"][0]
+            if track:
+                pl5.remove(track)
+            pl_dur -= files_list[track][0]
+            window['pl5'].update(pl5)
+            window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
 
-        # Remove items to playlist sections:
+        # Add items to playlist sections:
 
         elif event == "add1":
             tracks = values['-LIST-']
             for track in tracks:
                 pl1.append(track)
                 pl_dur += files_list[track][0]
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl1'].update(pl1)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
         elif event == "add2":
@@ -142,8 +184,6 @@ def main():
             for track in tracks:
                 pl2.append(track)
                 pl_dur += files_list[track][0]
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl2'].update(pl2)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
         elif event == "add3":
@@ -151,8 +191,6 @@ def main():
             for track in tracks:
                 pl3.append(track)
                 pl_dur += files_list[track][0]
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl3'].update(pl3)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
         elif event == "add4":
@@ -160,8 +198,6 @@ def main():
             for track in tracks:
                 pl4.append(track)
                 pl_dur += files_list[track][0]
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl4'].update(pl4)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
         elif event == "add5":
@@ -169,37 +205,40 @@ def main():
             for track in tracks:
                 pl5.append(track)
                 pl_dur += files_list[track][0]
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl5'].update(pl5)
             window['pld'].update(f"Playlist duration: {pl_dur//60} min. {pl_dur-(pl_dur//60)*60} sec.")
+
+        # Buttons UP and DOWN temporary doesn't work because of issue in .GetIndexes method in GUI framework
+
+        elif event == "up1" and len(values['pl1']):
+            index = window["pl1"].GetIndexes()[0]
+            if index > 0:
+                pl1[index], pl1[index-1] = pl1[index-1], pl1[index]
+                window['pl1'].update(pl1)
+
+        elif event == "dn1" and len(values['pl1']):
+            index = window["pl1"].GetIndexes()[0]
+            if index < len(pl1)-1:
+                pl1[index], pl1[index+1] = pl1[index+1], pl1[index]
+                window['pl1'].update(pl1)
+
 
         # Shuffle items in playlist sections:
 
         elif event == "sh1":
             random.shuffle(pl1)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl1'].update(pl1)
         elif event == "sh2":
             random.shuffle(pl2)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl2'].update(pl2)
         elif event == "sh3":
             random.shuffle(pl3)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl3'].update(pl3)
         elif event == "sh4":
             random.shuffle(pl4)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl4'].update(pl4)
         elif event == "sh5":
             random.shuffle(pl5)
-            settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-            pg_actions.save_state(files_list, settings)
             window['pl5'].update(pl5)
 
         # Remove items from source section, optionally delete files:
@@ -225,8 +264,6 @@ def main():
                 for key in files_list:
                     src_dur += files_list[key][0]
                 file_names = sorted(list(files_list.keys()))
-                settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
-                pg_actions.save_state(files_list, settings)
                 window['-LIST-'].update(file_names)
                 window['td'].update(f"Total files duration: {src_dur//60} min. {src_dur-(src_dur//60)*60} sec.")
             else:
@@ -249,6 +286,8 @@ def main():
                 window['pld'].update(f"Playlist duration: {pl_dur // 60} min. {pl_dur - (pl_dur // 60) * 60} sec.")
             else:
                 continue
+        settings = {"path": path, "pl1": pl1, "pl2": pl2, "pl3": pl3, "pl4": pl4, "pl5": pl5}
+        pg_actions.save_state(files_list, settings)
     window.close()
 
 
